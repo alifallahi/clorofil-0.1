2fab3fdaa59216dc49e7a84c4c2c3277b57324dd
Ali Fallahi <social@1pine.ir>
2017-01-07 00:56:26 +0330

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function slide01(){
+    document.getElementById("slideshow").style.backgroundImage="url(images/Slide01.jpg)";
+}
+function slide02(){
+    document.getElementById("slideshow").style.backgroundImage="url(images/Slide02.jpg)";
+}
+function slide03(){
+    document.getElementById("slideshow").style.backgroundImage="url(images/Slide03.jpg)";
+}
+
